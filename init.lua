9b8d1f1138f10b02cbfb3b11e6053372f7523718
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2023-11-12 22:54:25 +0000

    add normal mode Gvdiffsplit

--- init.lua
+++ init.lua
@@ -424,10 +424,11 @@ vim.keymap.set('n', '<leader>tf', require('nvim-tree.api').tree.focus, { noremap
 vim.keymap.set('n', '<leader>z', require('maximize').toggle, { desc = 'maximi[Z]e'})
 
 -- diff shortcuts
+vim.keymap.set('n', 'gv', '<cmd>Gvdiffsplit<cr>', { desc = '[G]it [V]diffsplit (normal mode)' })
 vim.keymap.set('n', '<leader>gv', '<cmd>Gvdiffsplit<cr>', { desc = '[G]it [V]diffsplit' })
-vim.keymap.set('n', '<leader>dgb', '<cmd>diffget BASE<cr>', { desc = '[D]iff [G]et [B]ASE' })
-vim.keymap.set('n', '<leader>dgl', '<cmd>diffget LOCAL<cr>', { desc = '[D]iff [G]et [L]OCAL' })
-vim.keymap.set('n', '<leader>dgm', '<cmd>diffget REMOTE<cr>', { desc = '[D]iff [G]et [R]EMOTE' })
+vim.keymap.set('n', 'dgb', '<cmd>diffget BASE<cr>', { desc = '[D]iff [G]et [B]ASE' })
+vim.keymap.set('n', 'dgl', '<cmd>diffget LOCAL<cr>', { desc = '[D]iff [G]et [L]OCAL' })
+vim.keymap.set('n', 'dgm', '<cmd>diffget REMOTE<cr>', { desc = '[D]iff [G]et [R]EMOTE' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
