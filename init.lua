b60c207b0e2fbde81653747ed4699dc4536c8f2e
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-01-20 15:17:46 +0000

    <C-\> to use copilot suggestion one word at a time

--- init.lua
+++ init.lua
@@ -919,3 +919,11 @@ require('highlight-undo').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+local function SuggestOneWord()
+  local suggestion = vim.fn['copilot#Accept']("")
+  local bar = vim.fn['copilot#TextQueuedForInsertion']()
+  return vim.fn.split(bar,  [[[ .]\zs]])[1]
+end
+
+vim.keymap.set('i', '<C-\\>', SuggestOneWord, {expr = true, remap = false})
