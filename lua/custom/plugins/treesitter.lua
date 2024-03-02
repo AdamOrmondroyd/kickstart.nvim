45c8dc30514a72713469e29c08f99bb4c564bf0e
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-03-02 22:41:37 +0000

    tidy up treesitter.lua

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,10 +1,13 @@
--- [[ Configure Treesitter ]]
+-- Treesitter
 -- See `:help nvim-treesitter`
+
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'fortran', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'fortran', 'go', 'lua', 'python',
+                         'rust', 'tsx', 'javascript', 'typescript',
+                         'vimdoc', 'vim', 'bash' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
@@ -71,6 +74,7 @@ vim.defer_fn(function()
     },
   }
 end, 0)
+
 return {
   -- Fuzzy Finder (files, lsp, etc)
   -- Highlight, edit, and navigate code
