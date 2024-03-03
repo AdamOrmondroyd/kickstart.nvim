ca7137b4575db6ea4b06704a5fff7e860e9c7cba
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-03-03 00:07:09 +0000

    replace snippet with lua

--- init.lua
+++ init.lua
@@ -285,6 +285,9 @@ local servers = {
       telemetry = { enable = false },
       -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
       -- diagnostics = { disable = { 'missing-fields' } },
+      completion = {
+        callSnipped = 'Replace',
+      }
     },
   },
 }
