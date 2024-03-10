d84d184e59b35b18c64f5850e8da2b4c0f1c7406
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-03-10 14:12:47 +0000

    move nvim-tree recommended config into config

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -8,6 +8,11 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
+      -- recommended todisable netrw at the
+      -- very start of your init.lua for filetree
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
       vim.keymap.set('n', '<leader>tt', require('nvim-tree.api').tree.toggle, { desc = '[T]ree [T]oggle' })
       vim.keymap.set('n', '<leader>tc', require('nvim-tree.api').tree.close, { noremap=true, desc = '[T]ree [C]lose' })
       vim.keymap.set('n', '<leader>tr', require('nvim-tree.api').tree.reload, { noremap=true, desc = '[T]ree [R]efresh' })
