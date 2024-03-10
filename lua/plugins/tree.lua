7e4e261b9c7376d291553ce4d40435f08c667f9b
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-03-10 21:08:31 +0000

    fix nvim-tree indentation

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,22 +1,22 @@
--- nvim-tree 
---
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      -- recommended todisable netrw at the
-      -- very start of your init.lua for filetree
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
+  --
+  -- nvim-tree 
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    -- recommended todisable netrw at the
+    -- very start of your init.lua for filetree
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-      vim.keymap.set('n', '<leader>tt', require('nvim-tree.api').tree.toggle, { desc = '[T]ree [T]oggle' })
-      vim.keymap.set('n', '<leader>tc', require('nvim-tree.api').tree.close, { noremap=true, desc = '[T]ree [C]lose' })
-      vim.keymap.set('n', '<leader>tr', require('nvim-tree.api').tree.reload, { noremap=true, desc = '[T]ree [R]efresh' })
-      vim.keymap.set('n', '<leader>tf', require('nvim-tree.api').tree.focus, { noremap=true, desc = '[T]ree [F]ocus' })
-      require("nvim-tree").setup {}
-    end,
+    vim.keymap.set('n', '<leader>tt', require('nvim-tree.api').tree.toggle, { desc = '[T]ree [T]oggle' })
+    vim.keymap.set('n', '<leader>tc', require('nvim-tree.api').tree.close, { noremap=true, desc = '[T]ree [C]lose' })
+    vim.keymap.set('n', '<leader>tr', require('nvim-tree.api').tree.reload, { noremap=true, desc = '[T]ree [R]efresh' })
+    vim.keymap.set('n', '<leader>tf', require('nvim-tree.api').tree.focus, { noremap=true, desc = '[T]ree [F]ocus' })
+    require("nvim-tree").setup {}
+  end,
 }
