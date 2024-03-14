3a991c115e283b6944b6af4a351066004abedf2a
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-03-14 22:57:48 +0000

    add markdown to treesitter ensured

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ vim.defer_fn(function()
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = { 'c', 'cpp', 'fortran', 'go', 'lua', 'python',
                          'rust', 'tsx', 'javascript', 'typescript',
-                         'vimdoc', 'vim', 'bash' },
+                         'vimdoc', 'vim', 'bash', 'markdown' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
