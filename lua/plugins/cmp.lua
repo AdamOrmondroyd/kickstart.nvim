00666b06daa4a3110882030ce74145e2040116e0
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-03-28 20:45:36 +0000

    C-e to close cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -49,6 +49,7 @@ return {
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-e>'] = cmp.mapping.close(),
         ['<C-Space>'] = cmp.mapping.complete {},
         ['<Tab>'] = cmp.mapping.confirm {
           behavior = cmp.ConfirmBehavior.Replace,
