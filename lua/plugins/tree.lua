a18ac4525328d28cf7adfbfa947d10b10fa160bb
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-04-01 19:48:32 +0100

    remove shortcut for tree close

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -14,7 +14,6 @@ return {
     vim.g.loaded_netrwPlugin = 1
 
     vim.keymap.set('n', '<leader>tt', require('nvim-tree.api').tree.toggle, { desc = '[T]ree [T]oggle' })
-    vim.keymap.set('n', '<leader>tc', require('nvim-tree.api').tree.close, { noremap=true, desc = '[T]ree [C]lose' })
     vim.keymap.set('n', '<leader>tr', require('nvim-tree.api').tree.reload, { noremap=true, desc = '[T]ree [R]efresh' })
     vim.keymap.set('n', '<leader>tf', require('nvim-tree.api').tree.focus, { noremap=true, desc = '[T]ree [F]ocus' })
     require("nvim-tree").setup {}
