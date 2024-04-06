b679a5d25e613a0db1a218e1f2926405ed2e4cbf
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-04-06 22:17:07 +0100

    remove pointless comment

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -8,7 +8,6 @@ return {
   config = function()
     local todo_comments = require("todo-comments")
 
-    -- set keymaps
     local keymap = vim.keymap
     keymap.set("n", "]t", todo_comments.jump_next, { desc = "Next todo" })
     keymap.set("n", "[t", todo_comments.jump_prev, { desc = "Prev todo" })
