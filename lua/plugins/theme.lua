b7dc442692d65c48dded9967802d9c555e698d99
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-04-19 16:51:08 +0100

    add obsidian plugin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -15,6 +15,7 @@ return {
       ['<leader>d'] = { name = '[D]ocument/[D]iff', _ = 'which_key_ignore' },
       ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
       ['<leader>h'] = { name = 'Git [H]unk/[H]ighlight', _ = 'which_key_ignore' },
+      ['<leader>o'] = { name = '[O]bsidian', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename/[R]estore', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>t'] = { name = '[T]ree/[T]ab', _ = 'which_key_ignore' },
