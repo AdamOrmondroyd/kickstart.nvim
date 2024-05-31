3cb8ce5c7bcee45c45d412dc3a7c852d803be65e
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-05-31 23:52:37 +0100

    add trouble.nvim

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -20,6 +20,7 @@ return {
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>t'] = { name = '[T]ree/[T]ab', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      ['<leader>x'] = { name = '[X]rouble', _ = 'which_key_ignore' },
     }
     -- register which-key VISUAL mode
     -- required for visual <leader>hs (hunk stage) to work
