247541f4bf67e37fe92ddd82603820feb087c18a
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2024-06-01 00:29:17 +0100

    tidy up builtin in telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,6 +21,7 @@ return {
       },
     },
     config = function()
+      local builtin = require('telescope.builtin')
       require('telescope').setup {
         defaults = {
           mappings = {
@@ -33,34 +34,34 @@ return {
       }
       pcall(require('telescope').load_extension, 'fzf')
       local keymap = vim.keymap
-      keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-      keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+      keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+      keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to telescope to change theme, layout, etc.
-        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
           previewer = false,
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
 
       local function telescope_live_grep_open_files()
-        require('telescope.builtin').live_grep {
+        builtin.live_grep {
           grep_open_files = true,
           prompt_title = 'Live Grep in Open Files',
         }
       end
       keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
-      keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
-      keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-      keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-      keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-      keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-      keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-      keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-      keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]resume' })
-      keymap.set('n', '<leader>sc', require('telescope.builtin').spell_suggest, { desc = '[S]pell [C]heck' })
-      keymap.set('n', '<leader>st', require('telescope.builtin').git_status, { desc = '[S]earch git s[T]atus' })
-      keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch [W]ord under cursor' })
+      keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
+      keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]resume' })
+      keymap.set('n', '<leader>sc', builtin.spell_suggest, { desc = '[S]pell [C]heck' })
+      keymap.set('n', '<leader>st', builtin.git_status, { desc = '[S]earch git s[T]atus' })
+      keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch [W]ord under cursor' })
       keymap.set('n', '<leader>sd', "<cmd>TodoTelescope<cr>", { desc = '[S]earch to[D]o' })
     end,
   },
