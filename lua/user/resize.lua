b8ec564963cd41b2e134b07c9a75ed8c2d3688c3
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-01-18 20:34:39 +0000

    --amend

--- lua/user/resize.lua
+++ lua/user/resize.lua
@@ -1,7 +1,7 @@
 -- Automactically resize windows on terminal resize
 
 vim.api.nvim_create_autocmd("VimResized", {
-  group = vim.api.nvim_create_augroup("WinResize", {clear = true}),
+  group = vim.api.nvim_create_augroup("WinResize", { clear = true }),
   pattern = "*",
   command = "wincmd =",
   desc = "Resize windows on terminal resize."
