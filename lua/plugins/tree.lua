20c9da1602504d4c90cbc054647344018fddb17e
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-01-18 20:31:54 +0000

    --amend

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -14,10 +14,11 @@ return {
     vim.g.loaded_netrwPlugin = 1
 
     vim.keymap.set('n', '<leader>tt', require('nvim-tree.api').tree.toggle, { desc = '[T]ree [T]oggle' })
-    vim.keymap.set('n', '<leader>tr', require('nvim-tree.api').tree.reload, { noremap=true, desc = '[T]ree [R]efresh' })
-    vim.keymap.set('n', '<leader>tf', require('nvim-tree.api').tree.focus, { noremap=true, desc = '[T]ree [F]ocus' })
-    vim.keymap.set('n', '<leader>t.', '<cmd>NvimTreeFindFile<bar>NvimTreeOpen<CR>', { noremap=true, desc = '[T]ree [.]' })
-    require("nvim-tree").setup ({
+    vim.keymap.set('n', '<leader>tr', require('nvim-tree.api').tree.reload, { noremap = true, desc = '[T]ree [R]efresh' })
+    vim.keymap.set('n', '<leader>tf', require('nvim-tree.api').tree.focus, { noremap = true, desc = '[T]ree [F]ocus' })
+    vim.keymap.set('n', '<leader>t.', '<cmd>NvimTreeFindFile<bar>NvimTreeOpen<CR>',
+      { noremap = true, desc = '[T]ree [.]' })
+    require("nvim-tree").setup({
       view = {
         side = "right",
         adaptive_size = true,
