db446dc3e3c1c728679807e7808e909635314320
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-01-18 19:00:13 +0000

    lualine depends on devicons

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -48,6 +48,9 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
     opts = {
       options = {
         icons_enabled = true,
