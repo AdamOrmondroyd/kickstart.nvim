4b44384389f1fc698ed174dd5d82a21b5cec2649
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-06-25 14:47:44 +0100

    change copilot suggestion colour to blue

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -18,5 +18,15 @@ return {
         markdown = true,
       },
     },
+    config = function(_, opts)
+      -- 1) set copilot up with your opts
+      require("copilot").setup(opts)
+      -- 2) override the inline suggestion colour
+      vim.api.nvim_set_hl(0, "CopilotSuggestion", {
+        fg     = "#aecfd9",  -- your desired colour
+        bg     = "NONE",     -- or whatever
+        italic = true,       -- optional
+      })
+    end,
   },
 }
