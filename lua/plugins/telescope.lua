9fac506be4f4d22dc5d8c9f70977a8278db3785e
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-07-23 18:27:05 +0100

    fix keymap clash sG sF

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -53,7 +53,7 @@ return {
       keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = '[S]earch [/] in Open Files' })
       keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      keymap.set('n', '<leader>sG', builtin.git_files, { desc = '[S]earch [G]it files' })
+      keymap.set('n', '<leader>sF', builtin.git_files, { desc = '[S]earch [G]it files' })
       keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
