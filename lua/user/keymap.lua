ebb45460229dbfe991886332e92e58070af42194
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-09-13 19:11:56 +0100

    remove alpha and bufferline

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -31,4 +31,8 @@ vim.keymap.set('n', '<leader>dgr', '<cmd>diffget REMOTE<cr>', { desc = '[D]iff [
 -- highlight search
 vim.keymap.set('n', '<leader>hl', '<cmd>set hlsearch!<cr>', { desc = '[H]igh [L]ight search' })
 
+-- tabs
+vim.keymap.set("<leader>tn", "<cmd>tabnew<cr>", {desc = "[T]ab [N]ew" })
+vim.keymap.set( "<leader>tc", "<cmd>tabclose<cr>", { desc = "[T]ab [C]lose" })
+
 return {}
