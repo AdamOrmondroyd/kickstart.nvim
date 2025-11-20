f55152fb245bf1dc2e4d53ee3a0222acdfb8201c
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-11-20 16:23:06 +0000

    fix for deprecated goto_prev/next

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -16,8 +16,8 @@ vim.keymap.set('n', ';', '<C-w>', { noremap = true })
 vim.keymap.set('t', '<esc>', '<C-\\><C-N>', { silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump({count=-1, float=true}) end, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', function() vim.diagnostic.jump({count=1, float=true}) end, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
