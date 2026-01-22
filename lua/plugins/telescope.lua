02196f7776d28187c16be6a8267c24ccb098acbf
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2026-01-22 17:16:27 +0000

    update telescope and lazy-lock

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
+    version = '0.2.*',  -- latest release tag
     dependencies = {
       'nvim-lua/plenary.nvim',
       'folke/todo-comments.nvim',
