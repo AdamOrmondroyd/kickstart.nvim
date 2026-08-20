4dbd7c0bd3531079ebd35ca6e15fbd83018c85a8
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2026-08-20 11:10:49 +0100

    use C-n and C-p to move through options in snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -46,8 +46,24 @@ return {
         completeopt = 'menu,menuone,noinsert',
       },
       mapping = cmp.mapping.preset.insert {
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping(function(fallback)
+          if luasnip.choice_active() then
+            luasnip.change_choice(1)
+          elseif cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<C-p>'] = cmp.mapping(function(fallback)
+          if luasnip.choice_active() then
+            luasnip.change_choice(-1)
+          elseif cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-e>'] = cmp.mapping.close(),
